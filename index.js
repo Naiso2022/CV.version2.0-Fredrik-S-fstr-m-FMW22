05a6227d73b956131ccb014ae468b750b14a3999
Fredrik Säfström <fredrik.safstrom@hotmail.se>
2022-11-06 22:29:05 +0100

    Fixed functions one-five

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const ulListThree = document.querySelector(".ulListThree")
 const ulListFour = document.querySelector(".ulListFour") 
 const ulListFive = document.querySelector(".ulListFive") 
 
+const workexperience = document.querySelector('.workexperience');
+const education = document.querySelector('.education');
+const specialCertificate = document.querySelector('.specialCertificate');
+const voluntaryWork = document.querySelector('.voluntaryWork');
+const otherMerits = document.querySelector('.otherMerits');
 
+const workexperienceDisplay = document.querySelector('.workexperience-display');
+const educationDisplay = document.querySelector('.education-display');
+const specialCertificateDisplay = document.querySelector('.special-certificate-display');
+const voluntaryWorkDisplay = document.querySelector('.voluntary-work-display');
+const otherMeritsDisplay = document.querySelector('.other-merits-display');
+
+workexperience.addEventListener('click', one);
+education.addEventListener('click', two);
+specialCertificate.addEventListener('click', three);
+voluntaryWork.addEventListener('click', four);
+otherMerits.addEventListener('click', five);
 
 async function jump() {
     const url = "index.json";
@@ -15,20 +31,9 @@ async function jump() {
         let data = await response.json();
         
         data.Workexperience.forEach(function(element) {
-            
-            let liStyle;
-            if(element.year) {
-                liStyle = "done"; 
-            } else {
-                liStyle = "undone"
-            }
-
-        
-
-
-            ulListOne.innerHTML +=`
+        ulListOne.innerHTML +=`
         <li class="moje">
-            ${element.year}:  <span class="${liStyle}">${element.titel}, </span>${element.company}, ${element.city}.
+            ${element.year}: ${element.titel}, ${element.company}, ${element.city}.
             </li>
             `;
         })
@@ -48,20 +53,10 @@ async function jumpa() {
         let data = await response.json();
         
         data.Education.forEach(function(element) {
-            
-            let liStyle;
-            if(element.year) {
-                liStyle = "done"; 
-            } else {
-                liStyle = "undone"
-            }
-
-        
-
 
             ulListTwo.innerHTML +=`
         <li class="moje">
-            ${element.year}:  <span class="${liStyle}">${element.education}, </span>${element.school}, ${element.city}.
+            ${element.year}: ${element.education}, ${element.school}, ${element.city}.
             </li>
             `;
         })
@@ -78,21 +73,17 @@ async function jumpan() {
     if(response.ok) {
         let data = await response.json();
         
-        data.Education.forEach(function(element) {
+        data.SpecialCertificate.forEach(function(element) {
             
-            let liStyle;
-            if(element.year) {
-                liStyle = "done"; 
-            } else {
-                liStyle = "undone"
-            }
-
-        
-
-
             ulListThree.innerHTML +=`
-        <li class="moje">
-            ${element.year}:  <span class="${liStyle}">${element.education}, </span>${element.school}, ${element.city}.
+            <li class="moje">
+            ${element.drivinglicence}.
+            </li>
+            <li class="moje">
+            ${element.coachEducation}.
+            </li>
+            <li class="moje">
+            ${element.refereeEducation}.
             </li>
             `;
         })
@@ -101,110 +92,86 @@ async function jumpan() {
 
 jumpan();
 
+async function jumpans() {
+    const url = "index.json";
+    let response = await fetch(url);
+   
 
+    if(response.ok) {
+        let data = await response.json();
+        
+        data.VoluntaryWork.forEach(function(element) {
 
-
-
-
-
-const workexperience = document.querySelector('.workexperience');
-const education = document.querySelector('.education');
-const specialCertificate = document.querySelector('.specialCertificate');
-const voluntaryWork = document.querySelector('.voluntaryWork');
-const otherMerits = document.querySelector('.otherMerits');
-
-const workexperienceDisplay = document.querySelector('.workexperience-display');
-const educationDisplay = document.querySelector('.education-display');
-const specialCertificateDisplay = document.querySelector('.special-certificate-display');
-const voluntaryWorkDisplay = document.querySelector('.voluntary-work-display');
-const otherMeritsDisplay = document.querySelector('.other-merits-display');
-
-
-
-workexperience.addEventListener('click', etta);
-education.addEventListener('click', tvåa);
-specialCertificate.addEventListener('click', Trea);
-voluntaryWork.addEventListener('click', fyra);
-otherMerits.addEventListener('click', fem);
-
-
-
-function etta(){
-    //workexperienceDisplay.classList.add("workexperience-display")
-    educationDisplay.classList.remove("education-display")
-    specialCertificateDisplay.classList.remove("special-certificate-display")
-    voluntaryWorkDisplay.classList.remove("voluntary-work-display")
-    otherMeritsDisplay.classList.remove("other-merits-display")
+            ulListFour.innerHTML +=`
+        <li class="moje">
+            ${element.year} ${element.work}
+            </li>
+            `;
+        })
+}
 }
 
+jumpans();
 
-function tvåa(){
-    workexperienceDisplay.classList.toggle("workexperience-display")
-    //educationDisplay.classList.add("education-display")
-    specialCertificateDisplay.classList.remove("special-certificate-display")
-    voluntaryWorkDisplay.classList.remove("voluntary-work-display")
-    otherMeritsDisplay.classList.remove("other-merits-display")
+
+async function jumpanse() {
+    const url = "index.json";
+    let response = await fetch(url);
+   
+
+    if(response.ok) {
+        let data = await response.json();
+        
+        data.OtherMerits.forEach(function(element) {
+            
+            ulListFive.innerHTML +=`
+        <li class="moje">
+            ${element.year} ${element.work} 
+            </li>
+            `;
+        })
+}
 }
 
-function trea(){
+jumpanse();
+
+function one(){
     workexperienceDisplay.classList.remove("workexperience-display")
+    educationDisplay.classList.add("education-display")
+    specialCertificateDisplay.classList.add("special-certificate-display")
+    voluntaryWorkDisplay.classList.add("voluntary-work-display")
+    otherMeritsDisplay.classList.add("other-merits-display")
+}
+
+
+function two(){
+    workexperienceDisplay.classList.add("workexperience-display")
     educationDisplay.classList.remove("education-display")
-    //specialCertificateDisplay.classList.remove("special-certificate-display")
+    specialCertificateDisplay.classList.add("special-certificate-display")
+    voluntaryWorkDisplay.classList.add("voluntary-work-display")
+    otherMeritsDisplay.classList.add("other-merits-display")
+}
+
+function three(){
+    workexperienceDisplay.classList.add("workexperience-display")
+    educationDisplay.classList.add("education-display")
+    specialCertificateDisplay.classList.remove("special-certificate-display")
+    voluntaryWorkDisplay.classList.add("voluntary-work-display")
+    otherMeritsDisplay.classList.add("other-merits-display")
+}
+
+function four(){
+    workexperienceDisplay.classList.add("workexperience-display")
+    educationDisplay.classList.add("education-display")
+    specialCertificateDisplay.classList.add("special-certificate-display")
     voluntaryWorkDisplay.classList.remove("voluntary-work-display")
+    otherMeritsDisplay.classList.add("other-merits-display")
+}
+
+function five(){
+    workexperienceDisplay.classList.add("workexperience-display")
+    educationDisplay.classList.add("education-display")
+    specialCertificateDisplay.classList.add("special-certificate-display")
+    voluntaryWorkDisplay.classList.add("voluntary-work-display")
     otherMeritsDisplay.classList.remove("other-merits-display")
-}
-
-function fyra(){
-        workexperienceDisplay.classList.remove("workexperience-display")
-        educationDisplay.classList.remove("education-display")
-        specialCertificateDisplay.classList.remove("special-certificate-display")
-        //voluntaryWorkDisplay.classList.remove("voluntary-work-display")
-        otherMeritsDisplay.classList.remove("other-merits-display")
-}
-
-function fem(){
-        workexperienceDisplay.classList.remove("workexperience-display")
-        educationDisplay.classList.remove("education-display")
-        specialCertificateDisplay.classList.remove("special-certificate-display")
-        voluntaryWorkDisplay.classList.remove("voluntary-work-display")
-        //otherMeritsDisplay.classList.remove("other-merits-display")
-}
-
-
-
-
-
-function stonechoice(){
-
-let computerchoice = computerturn();
-
-if (computerchoice === 2 ) {
-    humananswer.textContent = 'Din gissning är sten.';
-    computeranswer.textContent = 'Datorns gissning är sax.';
-    winner.textContent = 'Du vann!';
-    winner.style.backgroundColor = 'lightgreen';
-    humancounter++;
-    humanscore.textContent = humancounter;
-    
-}
-
-else if (computerchoice === 3){
-    humananswer.textContent = 'Din gissning är sten.';
-    computeranswer.textContent = 'Datorns gissning är påse.';
-    winner.textContent = 'Datorn vann!';
-    winner.style.backgroundColor = 'red';
-    computercounter++;
-    computerscore.textContent = computercounter;
-
-}
-
-else if (computerchoice === 1){
-    humananswer.textContent = 'Din gissning är sten.';
-    computeranswer.textContent = 'Datorns gissning är sten.';
-    winner.textContent = 'Oavgjort!';
-    winner.style.border = 'solid black';
-    winner.style.backgroundColor = 'white';
-    
-}
-
 }
